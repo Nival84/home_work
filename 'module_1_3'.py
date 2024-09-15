63fca88152f7b1e58eb6d08c2ae16793126191b2
Nival84 <rednival1984@gmail.com>
2024-09-15 14:51:14 +0300

    Add files via upload

---
+++ 'module_1_3'.py
@@ -0,0 +1,8 @@
+name = 'Alexandr'
+print("name:", name)
+age = 39
+print('age:', age)
+age = age + 0.5
+print('new_age', age)
+is_student = True
+print('is_student:', is_student)
---
+++ test3.py
@@ -0,0 +1,6 @@
+#Практическое задание по теме "Переменные
+Number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name = 'python'
+Time_for_one_task = Number_of_hours_spent/Number_of_completed_DZ
+print("Курс:", Course_name,',' "всего задач:", Number_of_completed_DZ,','"затрачено часов:",Number_of_hours_spent,","'среднее время выполнения', Time_for_one_task)
